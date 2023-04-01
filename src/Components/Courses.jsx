095141da91fe9524a71c18dc52ff594dd8b2d81a
Chzmo <chisomomoyo10@gmail.com>
2023-04-01 21:42:27 +0200

    Working on Courses Section

--- src/Components/Courses.jsx
+++ src/Components/Courses.jsx
@@ -8,17 +8,17 @@ function Courses() {
   }, [course])
   
   return (
-    <div id='courses'>
+    <div id='courses' className='py-12 px-7'>
       <h2 className="text-3xl font-bold">A broad selection of courses</h2>
       <p className='pt-4 text-lg'>Choose from 213,000 online video courses with new additions published every month</p>
-      <div className="flex gap-3">
-        <p onClick={(e) => setCourse(e.target.innerText)} className='font-bold text-lg text-black hover:text-slate-500'>Python</p>
-        <p onClick={(e) => setCourse(e.target.innerText)} className='font-bold text-lg text-black hover:text-slate-500'>Excel</p>
-        <p onClick={(e) => setCourse(e.target.innerText)} className='font-bold text-lg text-black hover:text-slate-500'>Web Development</p>
-        <p onClick={(e) => setCourse(e.target.innerText)} className='font-bold text-lg text-black hover:text-slate-500'>Javascript</p>
-        <p onClick={(e) => setCourse(e.target.innerText)} className='font-bold text-lg text-black hover:text-slate-500'>Data Science</p>
-        <p onClick={(e) => setCourse(e.target.innerText)} className='font-bold text-lg text-black hover:text-slate-500'>AWS Certification</p>
-        <p onClick={(e) => setCourse(e.target.innerText)} className='font-bold text-lg text-black hover:text-slate-500'>Drawing</p>
+      <div className="flex gap-3 pt-3">
+        <p onClick={(e) => setCourse(e.target.innerText)} className='font-bold text-lg text-black '>Python</p>
+        <p onClick={(e) => setCourse(e.target.innerText)} className='font-bold text-lg hover:text-black text-slate-500'>Excel</p>
+        <p onClick={(e) => setCourse(e.target.innerText)} className='font-bold text-lg hover:text-black text-slate-500'>Web Development</p>
+        <p onClick={(e) => setCourse(e.target.innerText)} className='font-bold text-lg hover:text-black text-slate-500'>Javascript</p>
+        <p onClick={(e) => setCourse(e.target.innerText)} className='font-bold text-lg hover:text-black text-slate-500'>Data Science</p>
+        <p onClick={(e) => setCourse(e.target.innerText)} className='font-bold text-lg hover:text-black text-slate-500'>AWS Certification</p>
+        <p onClick={(e) => setCourse(e.target.innerText)} className='font-bold text-lg hover:text-black text-slate-500'>Drawing</p>
       </div>
     </div>
   )
