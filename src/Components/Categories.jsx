5088c70900e3549ad56ffa9111943ac9a2a51baf
Chzmo <chisomomoyo10@gmail.com>
2023-04-03 15:07:53 +0200

    Working on Top categories Section

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -6,7 +6,9 @@ import softwareImg from '../assets/images/category-it-and-software.jpg'
 import marketingImg from '../assets/images/category-marketing.jpg'
 import personalImg from '../assets/images/category-personal-development.jpg'
 import businessImg from '../assets/images/category-business.jpg'
+import musicImg from '../assets/images/category-music.jpg'
 import photographyImg from '../assets/images/category-photography.jpg'
+import { Link } from 'react-router-dom'
 
 const categoriesData = [
   {'image':designImg, 'category':'Designnig'},
@@ -16,20 +18,21 @@ const categoriesData = [
   {'image':personalImg, 'category':'Personal Development'},
   {'image':businessImg, 'category':'Business'},
   {'image':photographyImg, 'category':'Photopgraphy'},
+  {'image':musicImg, 'category':'Music'},
 ]
 
 function Categories() {
   return (
     <div id='testimonial' className='p-7 bg-white relative'>
       <h2 className='font-bold text-xl pt-6'>Top Categories</h2>
-      <div className='mt-7 columns-4'>
+      <div className='mt-7 columns-4 gap-4 hover:cursor-pointer'>
         {categoriesData && 
           categoriesData.map((item, index) => {
             return (
-            <div key={index} className="">
-              <img src={item.image} alt="design" />
-              <h2>{item.category}</h2>
-            </div>
+              <Link key={index} className="pb-7">
+                <img src={item.image} alt="design" />
+                <h2 className='font-bold pt-2'>{item.category}</h2>
+              </Link>
             )
           })
         }
