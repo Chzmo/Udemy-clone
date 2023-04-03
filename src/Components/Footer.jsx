617fffc181c5fb42165ee0a657e46fc1ec418358
Chzmo <chisomomoyo10@gmail.com>
2023-04-03 23:23:03 +0200

    Working on Top categories Section

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,9 +5,9 @@ import { Link } from 'react-router-dom'
 function Footer() {
   return (
     <div className='px-7 pt-6 pb-9 bg-black text-white'>
-      <div className="flex justify-between">
-        <div className="grid-cols-3 grid gap-12">
-        <div className="flex flex-col gap-2">
+      <div className="flex flex-col-reverse gap-7 md:flex md:justify-between">
+        <div className="grid-cols-3 grid gap-1">
+          <div className="flex flex-col gap-2">
             <div><Link>Udemy Business</Link></div>
             <div><Link>Udemy Business</Link></div>
             <div><Link>Udemy Business</Link></div>
@@ -26,7 +26,7 @@ function Footer() {
             <div><Link>Udemy Business</Link></div>
           </div>
         </div>
-        <div className="">
+        <div className="w-32">
           <Link className='border border-white py-2 font-bold text-sm px-4 pr-6 flex gap-2 items-center'> <AiOutlineGlobal size={20}/> English</Link>
       </div>
       </div>
