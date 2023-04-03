0ec364b3977a3d8d13ff66c6db5d1f1705665449
Chzmo <chisomomoyo10@gmail.com>
2023-04-03 13:34:58 +0200

    Working on Top categories Section

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Categories() {
   return (
-    <div>Categories</div>
+    <div id='testimonial' className='p-7 bg-white h-32rem relative'>
+      <h2 className='font-bold text-xl pt-6'>How learners like you are achieving their goals</h2>
+      <div className='mt-7 h-full w-full relative'>
+          
+      </div>
+    </div>
   )
 }
 
