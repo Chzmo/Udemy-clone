1582192842e25ce990ba5b00bb987ed2e68f8ff3
Chzmo <chisomomoyo10@gmail.com>
2023-04-03 14:25:52 +0200

    Working on Top categories Section

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -4,44 +4,35 @@ import designImg from '../assets/images/category-design.jpg'
 import developmentImg from '../assets/images/category-development.jpg'
 import softwareImg from '../assets/images/category-it-and-software.jpg'
 import marketingImg from '../assets/images/category-marketing.jpg'
+import personalImg from '../assets/images/category-personal-development.jpg'
+import businessImg from '../assets/images/category-business.jpg'
+import photographyImg from '../assets/images/category-photography.jpg'
+
+const categoriesData = [
+  {'image':designImg, 'category':'Designnig'},
+  {'image':developmentImg, 'category':'Development'},
+  {'image':marketingImg, 'category':'Marketing'},
+  {'image':softwareImg, 'category':'IT and Software'},
+  {'image':personalImg, 'category':'Personal Development'},
+  {'image':businessImg, 'category':'Business'},
+  {'image':photographyImg, 'category':'Photopgraphy'},
+]
 
 function Categories() {
   return (
     <div id='testimonial' className='p-7 bg-white relative'>
       <h2 className='font-bold text-xl pt-6'>Top Categories</h2>
       <div className='mt-7 columns-4'>
-          <div className="">
-            <img src={designImg} alt="design" />
-            <h2>Design</h2>
-          </div>
-          <div className="">
-            <img src={developmentImg} alt="Development" />
-            <h2>Development</h2>
-          </div>
-          <div className="">
-            <img src={marketingImg} alt="Marketing" />
-            <h2>Marketing</h2>
-          </div>
-          <div className="">
-            <img src={softwareImg} alt="Software" />
-            <h2>IT and Software</h2>
-          </div>
-          <div className="">
-            <img src={designImg} alt="design" />
-            <h2>Personal development</h2>
-          </div>
-          <div className="">
-            <img src={developmentImg} alt="Development" />
-            <h2>Musiness</h2>
-          </div>
-          <div className="">
-            <img src={marketingImg} alt="Marketing" />
-            <h2>Music</h2>
-          </div>
-          <div className="">
-            <img src={softwareImg} alt="Software" />
-            <h2>Photography</h2>
-          </div>
+        {categoriesData && 
+          categoriesData.map((item, index) => {
+            return (
+            <div key={index} className="">
+              <img src={item.image} alt="design" />
+              <h2>{item.category}</h2>
+            </div>
+            )
+          })
+        }
       </div>
     </div>
   )
