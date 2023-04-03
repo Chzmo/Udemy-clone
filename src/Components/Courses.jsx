2fe776914e99a70ec8f76b0af979f24a8e794e99
Chzmo <chisomomoyo10@gmail.com>
2023-04-03 23:09:46 +0200

    Working on Top categories Section

--- src/Components/Courses.jsx
+++ src/Components/Courses.jsx
@@ -12,7 +12,7 @@ function Courses() {
   const [course, setCourse] = useState('Python')
 
   useEffect(() => {
-    console.log(course)
+    // console.log(course)
   }, [course])
 
   const Stars = (props) =>{
@@ -43,14 +43,14 @@ function Courses() {
         <p onClick={(e) => setCourse(e.target.innerText)} className='font-bold text-lg hover:text-black text-slate-500 cursor-pointer'>Drawing</p>
       </div>
       <div className="border border-slate-500 mt-3 p-7 ">
-        <h2 className="text-2xl font-bold">Expand your career opportunities with {course}</h2>
-        <p className='py-4 text-lg max-w-screen-lg'>
+        <h2 className="hidden md:flex text-2xl font-bold">Expand your career opportunities with {course}</h2>
+        <p className='hidden md:flex py-4 text-lg max-w-screen-lg'>
           Take one of Udemy’s range of Python courses and learn how to code using this 
           incredibly useful language. Its simple syntax and readability makes Python perfect for Flask, 
           Django, data science, and machine learning. You’ll learn how to build everything from games to 
           sites to apps. Choose from a range of courses that will appeal to
         </p>
-        <Link className='border border-black py-2 px-4 font-bold'>Explore {course}</Link>
+        <Link className='hidden md:inline-block border border-black py-2 px-4 font-bold'>Explore {course}</Link>
         <div className="flex gap-5 mt-7">
           <div>
             <div className="h-40 w-72">
@@ -63,19 +63,6 @@ function Courses() {
             <h2 className='w-64 font-bold text-xl'>The Complete Bootcamp From Zero To Hero..</h2>
             <p>Jose Name</p>
             <div className="flex gap-2"><h2>4.6</h2><Stars stars={5}/><p>(12,004)</p></div>
-            <p>$182.99</p>
-          </div>
-          <div>
-            <div className="h-40 w-72">
-              <img 
-                src={randomImage2} 
-                alt={course} 
-                className='h-40 w-72 bg-slate-100'
-              />
-            </div>
-            <h2 className='w-64 font-bold text-xl'>The Complete Bootcamp From Zero To Hero..</h2>
-            <p>Jose Name</p>
-            <div className="flex gap-2"><h2>4.6</h2><Stars stars={4}/><p>(12,004)</p></div>
             <p>$182.99</p>
           </div>
         </div>
