bacc595da9970a485a5017b8c3da5df6e7b2d764
Chzmo <chisomomoyo10@gmail.com>
2023-04-04 14:04:14 +0200

    Working on resposive hero

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,28 +5,35 @@ import { Link } from 'react-router-dom'
 function Footer() {
   return (
     <div className='px-7 pt-6 pb-9 bg-black text-white'>
-      <div className="flex flex-col-reverse gap-7 md:flex md:justify-between">
-        <div className="grid-cols-3 grid gap-1">
-          <div className="flex flex-col gap-2">
+      <div className="
+        flex flex-col-reverse gap-7 
+        sm:flex sm:flex-row sm:justify-between sm:gap-2/3
+      ">
+        <div className="
+          flex flex-col gap-4
+          sm:flex-row sm:gap-12 sm:justify-between
+        "
+      >
+          <div className="flex flex-col gap-1 sm:gap-2">
             <div><Link>Udemy Business</Link></div>
             <div><Link>Udemy Business</Link></div>
             <div><Link>Udemy Business</Link></div>
             <div><Link>Udemy Business</Link></div>
           </div>
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-1 sm:gap-2">
             <div><Link>Udemy Business</Link></div>
             <div><Link>Udemy Business</Link></div>
             <div><Link>Udemy Business</Link></div>
             <div><Link>Udemy Business</Link></div>
           </div>
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-1 sm:gap-2">
             <div><Link>Udemy Business</Link></div>
             <div><Link>Udemy Business</Link></div>
             <div><Link>Udemy Business</Link></div>
             <div><Link>Udemy Business</Link></div>
           </div>
         </div>
-        <div className="w-32">
+        <div className=" flex-none">
           <Link className='border border-white py-2 font-bold text-sm px-4 pr-6 flex gap-2 items-center'> <AiOutlineGlobal size={20}/> English</Link>
       </div>
       </div>
