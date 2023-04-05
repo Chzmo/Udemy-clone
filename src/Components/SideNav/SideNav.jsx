1d39870f815c21bf714ec76601f9ff7d1a3c1380
Chzmo <chisomomoyo10@gmail.com>
2023-04-05 12:45:28 +0200

    Working on sidenav

--- src/Components/SideNav/SideNav.jsx
+++ src/Components/SideNav/SideNav.jsx
@@ -5,22 +5,22 @@ function SideNav(props) {
   return (
     <>
       <div
-        className='absolute bg-black top-0 left-0 bottom-0 right-0 z-20 opacity-40'
+        className='static bg-black top-0 left-0 bottom-0 right-0 z-20 opacity-40 fixed' 
       >
 
       </div>
         <div 
           className='
-            absolute bg-white h-screen top-0 left-0 z-20 overflow-y-scroll fixed
+            static bg-white h-screen top-0 left-0 z-20 overflow-y-scroll fixed
             w-4/5 py-4 px-3
           '
           >
           side nav
         </div>
         <div 
-          className='absolute bg-white top-4 right-4 rounded-full flex items-center justify-center z-20 p-3'
+          className='static fixed bg-white top-3 right-3 rounded-full flex items-center justify-center z-20 p-3'
         >
-          <RiCloseFill />
+          <RiCloseFill size={20}/>
         </div>
     </>
   )
