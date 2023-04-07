486caf38284c8d574c521cd24a73762585175844
Chzmo <chisomomoyo10@gmail.com>
2023-04-08 01:03:55 +0200

    working on search

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -1,32 +1,52 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { TbPointFilled } from 'react-icons/tb'
-import { BsQuote } from 'react-icons/bs';
+import { BiChevronDown, BiFilter } from 'react-icons/bi';
 import { FaQuoteLeft, FaQuoteRight } from 'react-icons/fa';
 import Spinner from '../Components/Spinner';
 
 function Search() {
   const { searchTerm } = useParams();
-  const [searchResults, setSearchResults] = useState(null);
+  const [searchResults, setSearchResults] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
 
   return (
     <>
-      <div className="min-h-[400px] pt-16">
+      <div className="min-h-[400px] pt-10">
         {(!isLoading && searchResults) && 
           <>
-            <h2>849 results for {searchTerm}</h2>
+            <div className="flex flex-col px-5">
+              <p className=' flex items-center text-[2rem] font-bold text-slate-900 gap-3'>
+                10, 000 result for <span className='flex '><FaQuoteLeft size={13}/> {searchTerm} <FaQuoteRight size={13}/> </span> 
+              </p>
+              <div className="flex justify-between mt-5">
+                <div className="flex gap-2">
+                  <button className='flex p-3 border border-slate-800 gap-1 items-center font-bold hover:bg-slate-50'>
+                    <BiFilter size={24}/>
+                    <span>Filter</span>
+                  </button>
+                  <button className='flex p-3 border border-slate-800 gap-4 items-center hover:bg-slate-50'>
+                    <div className="flex flex-col items-start">
+                      <small className='font-semibold text-slate-700'>Sorted by</small>
+                      <span>Most reviewd</span>
+                    </div>
+                    <BiChevronDown size={24}/>
+                  </button>
+                </div>
+                <p className='font-bold text-slate-700 self-end text-[1.2rem]'>10,000 results</p>
+              </div>
+            </div>
           </>
         }
 
         { (!searchResults && !isLoading) &&
           <div className="flex flex-col px-7 gap-5">
-            <div className="flex flex-col">
+            <div className="flex flex-col pb-4">
               <p className='text-[1.7rem] font-bold text-slate-800'>Sorry, we couldn't find any results for </p>
               <p className='text-[1.7rem] font-bold text-slate-800 flex items-center'>
-                <FaQuoteLeft size={13} className='transform -scale-x-1 '/>
+                <FaQuoteLeft size={13} />
                 {searchTerm}
-                <FaQuoteRight size={13} className='transform -scale-x-1 '/>
+                <FaQuoteRight size={13}/>
               </p>
             </div>
 
