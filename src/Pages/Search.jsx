52ef89023aa9e8c09822af35aeb73aa81add2005
Chzmo <chisomomoyo10@gmail.com>
2023-04-07 22:53:24 +0200

    working on side nav

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 function Search() {
+  const {searchTerm} = useParams();
+  const [searchResults, setSearchResults] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+
   return (
     <>
       <div className="min-h-screen ">
-        
+        {(!isLoading && searchResults) && 
+          <>
+            <h2>849 results for {searchTerm}</h2>
+          </>
+        }
       </div>
     </>
   )
