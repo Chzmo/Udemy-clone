2f69a023fc44429360e45fbf4e214f48db1f7f9b
Chzmo <chisomomoyo10@gmail.com>
2023-04-09 10:43:25 +0200

    working on right side nav

--- src/Components/SideNav/RightSideNav.jsx
+++ src/Components/SideNav/RightSideNav.jsx
@@ -7,6 +7,7 @@ function RightSideNav({isRightNavOpen, setIsRightNavOpen}) {
         <div 
             onClick={()=> setIsRightNavOpen(!isRightNavOpen)}
             className={`
+                sm:hidden
                 static fixed z-20 ${isRightNavOpen? '':'hidden'} 
                 top-0 left-0 right-0 h-screen bg-black 
                 opacity-40 overflow-hidden
@@ -15,9 +16,12 @@ function RightSideNav({isRightNavOpen, setIsRightNavOpen}) {
         </div>
         <div className={`
             static fixed w-4/5 top-0 ${isRightNavOpen? 'right-0':'-right-full'}
-            bg-white z-20 h-screen duration-500
+            bg-white z-20 h-screen duration-500 overflow-y-scroll
         `}>
-            <div className="h-screen duration-500 relative h-full w-full px-3 overflow-y-scroll">
+            <div className="w-full p-3 shadow-md">
+                1 result
+            </div>
+            <div className="h-screen duration-500 relative h-full w-full px-3 ">
                 <Filter />
             </div>
         </div>
