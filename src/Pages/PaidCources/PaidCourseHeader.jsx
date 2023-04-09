60ea99878db401b7fa01413acf18f4843821966d
Chzmo <chisomomoyo10@gmail.com>
2023-04-09 23:15:40 +0200

    working on paid-courses

--- src/Pages/PaidCources/PaidCourseHeader.jsx
+++ src/Pages/PaidCources/PaidCourseHeader.jsx
@@ -30,10 +30,10 @@ function PaidCourseHeader() {
                     <Link>Python</Link>
                 </small>
             </div>
-            <div className="flex md:flex-row flex-col-reverse gap-4 w-[90%] md:flex md:gap-6 m-auto md:w-[81%] text-white py-7">
+            <div className="flex md:flex-row items-start flex-col-reverse gap-4 w-[90%] md:flex md:gap-6 m-auto md:w-[81%] text-white md:py-7">
                 <div 
                     className={`
-                        w-full md:w-4/5 m-auto gap-2
+                        w-full md:w-4/5 m-auto gap-2 self-end
                     `}
                 >
                     <small className="md:flex hidden w-3/4 gap-2 items-center text-purple-300 font-bold">
@@ -54,21 +54,24 @@ function PaidCourseHeader() {
                         <Link className='text-purple-300'>(458,574 ratings)</Link>
                         <p>1,702,710 students</p>
                     </div>
-                    <div className="flex gap-2 items-center"><p className='text-[.8rem] md:text-1rem]'>Created by </p ><Link className='text-purple-300'><small>Zaliro</small></Link></div>
+                    <div className="flex gap-2 items-center"><p className='text-[.8rem] md:text-1rem]'>Created by </p ><Link className='text-purple-300 flex items'><small>Zaliro</small></Link></div>
                     <div className="flex gap-4 items-center">
                     <small> Last updated 03/2021 </small>
                     <small> English</small>
                     </div>
                 </div>
 
-
                 <div className="w-full md:w-2/5  ">
-                    <div className=" w-full">
-                    <img 
-                        src={randomImage1} 
-                        alt={`course`} 
-                        className='h-40 w-full sm:h-[13rem] md:h-40 bg-slate-100 object-cover'
-                    />
+                    <div className="flex flex-col ">
+                        <div className="w-full  ">
+                            <img 
+                                src={randomImage1} 
+                                alt={`course`} 
+                                className='h-40 w-full sm:h-[13rem] md:h-40 border border-white bg-slate-100 object-cover'
+                            />
+                        </div>
+                        <div className="py-y static sticky fixed z-30  bg-white md:w-full text-black">
+                        </div>
                     </div>
                 </div>
             </div>
