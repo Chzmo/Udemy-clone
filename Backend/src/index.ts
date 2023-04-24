10781105a1487d61810361b8ea7e8373b79c9722
Chzmo <chisomomoyo10@gmail.com>
2023-04-24 14:07:44 +0200

    Configuring express

---
+++ Backend/src/index.ts
@@ -0,0 +1,24 @@
+// import * as dotenv from 'dotenv';
+// import express from 'express';
+// import cors from 'cors';
+
+
+// console.log(121212)
+// dotenv.config();
+
+// if (!process.env.PORT){
+//     process.exit(1);
+// }
+
+// const PORT : number = parseInt(process.env.PORT as string, 10)
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.listen(PORT, ()=>{
+//     console.log('Listening on port ', PORT)
+// })
+
+console.log('hello')
