a208ed7c2101032c9e4b7c8316eb1364b671b454
Chzmo <chisomomoyo10@gmail.com>
2023-04-28 12:22:10 +0200

    working on middleware

---
+++ Backend/prisma/migrations/20230428101858_init/migration.sql
@@ -0,0 +1,72 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Course" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "tittle" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "datePublished" DATETIME NOT NULL,
+    "authorId" INTEGER NOT NULL,
+    "price" REAL NOT NULL DEFAULT 0,
+    "revisedPrice" REAL NOT NULL,
+    "thumbnail" TEXT NOT NULL,
+    CONSTRAINT "Course_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Content" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "courseId" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Content_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "Course" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Requirements" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "courseId" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Requirements_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "Course" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Rating" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "value" REAL NOT NULL,
+    "review" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "UserOnCourseRating" (
+    "corseId" INTEGER NOT NULL,
+    "ratingId" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "assignedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "ratedBy" INTEGER NOT NULL,
+
+    PRIMARY KEY ("corseId", "ratingId", "userId"),
+    CONSTRAINT "UserOnCourseRating_corseId_fkey" FOREIGN KEY ("corseId") REFERENCES "Course" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "UserOnCourseRating_ratingId_fkey" FOREIGN KEY ("ratingId") REFERENCES "Rating" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "UserOnCourseRating_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Content_courseId_key" ON "Content"("courseId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Requirements_courseId_key" ON "Requirements"("courseId");
