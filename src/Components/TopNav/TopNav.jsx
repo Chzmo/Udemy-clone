20fc2cb0092412c6ecacba1b4fd05a5345d2b44e
Chzmo <chisomomoyo10@gmail.com>
2023-04-30 14:32:36 +0200

    working on category nav

--- src/Components/TopNav/TopNav.jsx
+++ src/Components/TopNav/TopNav.jsx
@@ -8,6 +8,7 @@ import { Link, useNavigate } from 'react-router-dom'
 import logo from '../../assets/logo/logo-udemy.svg'
 import SideNav from '../SideNav/SideNav'
 import SearchInput from '../../Pages/Search/SearchInput'
+import CategoryNav from './CategoryNav'
 
 
 function TopNav({globalState}) {
@@ -70,59 +71,7 @@ function TopNav({globalState}) {
                     >
                         Categories
                     </p>
-                    <div 
-                        className={`h-screen absolute border border-slate-300 min-w-64 top-12 z-10 bg-white -left-8 ${isCatergoryNavOpen? 'flex' : 'hidden' } flex ${!globalState.categories && 'hidden'}`}
-                        onMouseEnter={() => {setIsCatergoryNavOpen(true)}}
-                        onMouseLeave={() => {setIsCatergoryNavOpen(false)}}
-                    >
-                        <div className="h-screen w-64 flex flex-col hover:text-black py-4 gap-4">
-                            {globalState.categories && 
-                                globalState.categories?.map((category, index)=>{
-                                    return (
-                                        <Link 
-                                            key={index} 
-                                            className="px-3 hover:text-purple-800 flex items-center justify-between w-full"
-                                            onMouseEnter={() => setCourses(category.course)}
-                                        >
-                                            <p>{category?.title}</p> <BiChevronRight/>
-                                        </Link>
-                                    )
-                                })
-                            }
-                        </div>
-                        <div className={`h-screen w-64 border-l flex flex-col  top-0 py-4 gap-4 ${!courses && 'hidden'}`}>
-                            {courses && 
-                                courses?.map((course, index)=>{
-                                    return (
-                                        <Link key={index} className="px-3 hover:text-purple-800 flex items-center justify-between w-full">
-                                            <p>{course?.name}</p> <BiChevronRight/>
-                                        </Link>
-                                    )
-                                })
-                            }
-                        </div>
-
-                        <div className={`h-screen px-3 w-64 border-l flex flex-col gap-4 top-0 py-4 ${!topics && 'hidden'}`}>
-                            <h2 className='font-semibold text-sm text-slate-600 '>Popular topics</h2>
-                            {isLoadingTopics ? 
-                                <>
-                                    {[1,2,3,4,5,6,7].map(index => {
-                                        return (
-                                            <div key={index} className="animate-pulse w-full h-5 bg-slate-200"></div>
-                                        )
-                                    })}
-                                </>
-                                :
-                                <>
-                                    {[1,2,3,4,5,6,7,8].map(index => {
-                                        return (
-                                            <div className="hover:text-purple-800 flex items-center justify-between w-full"></div>
-                                        )
-                                    })}
-                                </>
-                            }
-                        </div>
-                    </div>
+                    <CategoryNav isCatergoryNavOpen={isCatergoryNavOpen}/>
                 </div>
                 <label 
                     htmlFor="search"
