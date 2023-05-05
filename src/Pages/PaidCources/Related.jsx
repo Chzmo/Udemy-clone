2feca28914d2fc4faa003d9de3d586c18771a056
Chzmo <chisomomoyo10@gmail.com>
2023-05-06 01:03:50 +0200

    added a related on courses

--- src/Pages/PaidCources/Related.jsx
+++ src/Pages/PaidCources/Related.jsx
@@ -1,22 +1,37 @@
 import React from 'react'
 import { BsHeart } from 'react-icons/bs';
 import { MdGroup, MdStarRate } from 'react-icons/md';
-const data = [  "Complete Python Bootcamp: From Zero to Hero in Python 3",  "The Complete Web Developer Course 2.0",  "React - The Complete Guide (incl Hooks, React Router, Redux)",  "Machine Learning A-Z™: Hands-On Python & R In Data Science",  "AWS Certified Solutions Architect - Associate 2022",  "The Complete Digital Marketing Course - 12 Courses in 1",  "The Complete Financial Analyst Course 2022",  "JavaScript: Understanding the Weird Parts",  "Ultimate Google Ads Training 2022: Profit with Pay Per Click",  "iOS 15 & Swift 5 - The Complete iOS App Development Course"]
-
+const courseTitles = [
+    { title: "Complete Python Bootcamp: From Zero to Hero in Python 3", language: "Python" },
+    { title: "The Complete Web Developer Course 2.0", language: "JavaScript" },
+    { title: "React - The Complete Guide (incl Hooks, React Router, Redux)", language: "TypeScript" },
+    { title: "Machine Learning A-Z™: Hands-On Python & R In Data Science", language: ["data",] },
+    { title: "AWS Certified Solutions Architect - Associate 2022", language: "AWS" },
+    { title: "The Complete Digital Marketing Course - 12 Courses in 1", language: "Marketing" },
+    { title: "The Complete Financial Analyst Course 2022", language: "Analyst" },
+    { title: "JavaScript: Understanding the Weird Parts", language: "JavaScript" },
+    { title: "Ultimate Google Ads Training 2022: Profit with Pay Per Click", language: "Google" },
+    { title: "iOS 15 & Swift 5 - The Complete iOS App Development Course", language: "Swift" }
+  ];
+  
 function Related() {
     return (
         <>
             <h2 className="bold font-bold text-2xl">Students also bought</h2>
             <div className="flex flex-col gap-4 w-full">
-                {data.map((title, index)=>{
+                {courseTitles.map((course, index)=>{
                     return (
                         <div key={`related-${index}`} className="flex gap-2 w-full">
                             <div className="h-16 w-16 bg-slate-300">
-                                <img className='h-full w-full object-cover' src="" alt="related" />
+                                <img 
+                                    className='h-full w-full object-cover' 
+                                    src= {`https://source.unsplash.com/80x80/?programming/${course?.language}`} 
+                                    alt="related" 
+                                />
                             </div>
                             <div className="flex-1 flex flex-wrap justify-between w-full sm:flex-row sm:flex-nowrap">
                                 <div className="flex flex-col gap-2 w-full">
-                                    <h2 className='font-bold text-lg max-w-[19rem]'>{title}</h2>
+                                    <h2 className='font-bold text-lg max-w-[19rem]'>{course.title}</h2>
                                     <small className="flex gap-3 items-center">
                                         <span className='text-green-900 font-semibold'>12 total hours</span>
                                         <span className=''>Updated 1/2023</span>
