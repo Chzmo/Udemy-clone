1da8454c8cdd0aef51cc202bbd3262f9797258c1
Chzmo <chisomomoyo10@gmail.com>
2023-05-06 14:20:16 +0200

    added instrutor section on courses

--- src/Pages/PaidCources/Instructor.jsx
+++ src/Pages/PaidCources/Instructor.jsx
@@ -17,7 +17,18 @@ function Instructor() {
                             Jonas Schmedtmann
                         </HashLink>
                     </div>
-                    <h2>Web Developer, Designer, and Teacher</h2>
+                    <h2 className='text-slate-700'>Web Developer, Designer, and Teacher</h2>
+                    <div className="flex gap-3">
+                        <div className="w-32 h-32 bg-slate-300 rounded-full mt-2">
+                            <img 
+                                src="" 
+                                alt={`Instructor`}
+                                className='h-full w-full rounded-full object-cover
+                                '
+                            />
+                        </div>
+                        <div className="flex flex-col"></div>
+                    </div>
                 </div>
                 <p className=' flex gap-1 items-center' onClick={ ()=> setIsOpen(!isOpen)}>
                     <small className='font-bold text-purple-700 cursor-pointer'>
