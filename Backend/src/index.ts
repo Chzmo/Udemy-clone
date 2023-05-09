48e7a3e817b533e9d6c5a14b4b076f8b6363c13f
Chzmo <chisomomoyo10@gmail.com>
2023-05-09 22:15:27 +0200

    workign on ratings

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,39 +1,47 @@
-import * as dotenv from 'dotenv';
-import express from 'express';
-import cors from 'cors';
-import { authMiddleware } from '../middleware/authMiddleware';
+import * as dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import { authMiddleware } from "../middleware/authMiddleware";
 
-import { userRouter, usersRouter, loginRouter, legisterRouter } from './routes/auth/auth.router';
-import { courseRouter, coursesRouter } from './routes/course.route';
+import {
+	userRouter,
+	usersRouter,
+	loginRouter,
+	legisterRouter,
+} from "./routes/auth/auth.router";
+import { courseRouter, coursesRouter } from "./routes/course.route";
 
-import { categoryRouter } from './routes/category.route'
+import { categoryRouter } from "./routes/category.route";
+import { ratingRouter } from "./routes/rating.route";
 
 dotenv.config();
 
-if (!process.env.PORT){
-    process.exit(1);
+if (!process.env.PORT) {
+	process.exit(1);
 }
 
-const PORT : number = parseInt(process.env.PORT as string, 10)
-
+const PORT: number = parseInt(process.env.PORT as string, 10);
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/user", authMiddleware, userRouter)
-app.use("/api/users", authMiddleware, usersRouter)
-app.use("/api/register", legisterRouter)
-app.use("/api/login", loginRouter)
+app.use("/api/user", authMiddleware, userRouter);
+app.use("/api/users", authMiddleware, usersRouter);
+app.use("/api/register", legisterRouter);
+app.use("/api/login", loginRouter);
 
 // COURSES ROUTES
-app.use("/api/courses", coursesRouter)
-app.use("/api/course", courseRouter)
+app.use("/api/courses", coursesRouter);
+app.use("/api/course", courseRouter);
 
 // CATEGORIES ROUTES
-app.use("/api/category", categoryRouter)
-app.use("/api/categories", categoryRouter)
+app.use("/api/category", categoryRouter);
+app.use("/api/categories", categoryRouter);
 
-app.listen(PORT, ()=>{
-    console.log('Listening on port ', PORT)
-})
+// RATING ROUTES
+app.use("/api/rating", ratingRouter);
+
+app.listen(PORT, () => {
+	console.log("Listening on port ", PORT);
+});
