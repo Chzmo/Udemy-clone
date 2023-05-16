f565fd6e5e4951eda777c04cebc21ca8fb1e4424
Chzmo <chisomomoyo10@gmail.com>
2023-05-16 18:08:48 +0200

    working on login functionality

--- src/Container/Container.jsx
+++ src/Container/Container.jsx
@@ -1,38 +1,40 @@
-import React, { useEffect, useState } from 'react'
-import { Outlet, useOutletContext } from 'react-router-dom';
-import { fetchData } from '../Utils/FetchData';
+import React, { useEffect, useState } from "react";
+import { Outlet, useOutletContext } from "react-router-dom";
+import { fetchData } from "../Utils/Query";
 
-import Footer from '../Components/Footer/Footer'
-import TopNav from '../Components/TopNav/TopNav';
+import TopNav from "../Components/TopNav/TopNav";
 
 function Container() {
-  const [categories, setCategories] = useState(null);
-  const [loading, setLoading] = useState(false);
+	const [categories, setCategories] = useState(null);
+	const [loading, setLoading] = useState(false);
 
-  const globalState = {
-    loading, setLoading, categories, setCategories
-  }
+	const globalState = {
+		loading,
+		setLoading,
+		categories,
+		setCategories,
+	};
 
-  async function getData(){
-    const data = await fetchData('/api/categories')
-    setCategories(data)
-  }
-  
-  useEffect(() => {
-    console.log('start')
-    getData().finally(()=> console.log('end'))
-  }, [])
+	async function getData() {
+		const data = await fetchData("/api/categories");
+		setCategories(data);
+	}
 
-  return (
-    <div >
-        <TopNav globalState={globalState}/>
-        <Outlet context={{globalState}}/>
-    </div>
-  )
+	useEffect(() => {
+		console.log("start");
+		getData().finally(() => console.log("end"));
+	}, []);
+
+	return (
+		<div>
+			<TopNav globalState={globalState} />
+			<Outlet context={{ globalState }} />
+		</div>
+	);
 }
 
-export default Container
+export default Container;
 
-export function useGlobalState(){
-  return useOutletContext()
+export function useGlobalState() {
+	return useOutletContext();
 }
