4778981023372ddcf692f47cfcfd7635b40630ed
Chzmo <chisomomoyo10@gmail.com>
2023-11-01 15:28:42 +0200

    adding topics to sections

--- api/src/index.ts
+++ api/src/index.ts
@@ -21,9 +21,9 @@ import { courseContentRouter } from "./routes/courseContent.route";
 
 dotenv.config();
 
-if (!process.env.PORT) {
-	process.exit(1);
-}
+// if (!process.env.PORT) {
+// 	process.exit(1);
+// }
 
 const PORT: number = parseInt(process.env.PORT as string, 10);
 const app = express();
