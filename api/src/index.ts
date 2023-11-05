97e968aa280d13b694ba53669dcc74390c50eb7c
Chzmo <chisomomoyo10@gmail.com>
2023-11-05 12:18:40 +0200

    added 404 error

--- api/src/index.ts
+++ api/src/index.ts
@@ -64,6 +64,10 @@ app.use("/api/categories", categoryRouter);
 // RATING ROUTES
 app.use("/api/rating", ratingRouter);
 
+app.use((req, res, next) => {
+	res.status(404).json({ error: "Not Found" });
+});
+
 app.listen(PORT, () => {
 	console.log("Listening on port ", PORT);
 });
