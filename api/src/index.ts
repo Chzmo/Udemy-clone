df185763ed659adfa2a4a8080f45c70ea10beb0e
Chzmo <chisomomoyo10@gmail.com>
2023-11-05 11:04:14 +0200

    updated index.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -22,9 +22,9 @@ import { courseRequirementsRouter } from "./routes/courseRequirements.route";
 
 dotenv.config();
 
-// if (!process.env.PORT) {
-// 	process.exit(1);
-// }
+if (!process.env.PORT) {
+	process.exit(1);
+}
 
 const PORT: number = parseInt(process.env.PORT as string, 10);
 const app = express();
