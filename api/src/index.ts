ab4a3a7837df3649ac5d2c5d10a400713282d3ce
Chzmo <chisomomoyo10@gmail.com>
2023-11-05 11:23:32 +0200

    updated index.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -20,11 +20,11 @@ import { courseContentRouter } from "./routes/courseContent.route";
 import { courseObjectivesRouter } from "./routes/objectives.route";
 import { courseRequirementsRouter } from "./routes/courseRequirements.route";
 
-dotenv.config();
+// dotenv.config();
 
-if (!process.env.PORT) {
-	process.exit(1);
-}
+// if (!process.env.PORT) {
+// 	process.exit(1);
+// }
 
 const PORT: number = parseInt(process.env.PORT as string, 10);
 const app = express();
